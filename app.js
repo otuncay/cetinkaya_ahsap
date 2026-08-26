d63605ede2a9e620fa154c8818ea44d902b365ca
Oğuzhan Tuncay <99074037+otuncay@users.noreply.github.com>
2026-08-26 11:09:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 
+console.log("Cetinkaya Ahşap uygulaması başlatıldı.");
+
+async function testSupabaseConnection() {
+
+    const { data, error } = await supabaseClient
+        .from("categories")
+        .select("*")
+        .limit(1);
+
+    if (error) {
+        console.error("Supabase bağlantı hatası:", error);
+        return;
+    }
+
+    console.log("Supabase bağlantısı başarılı:", data);
+}
+
+testSupabaseConnection();
