6511a6996c471612591eb12dbc5ce6d18ea18457
Oğuzhan Tuncay <99074037+otuncay@users.noreply.github.com>
2026-08-26 11:17:19 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://otuncay.github.io/cetinkaya_ahsap/#";
+const SUPABASE_URL = "https://rnghluubnrjzipvsbbuv.supabase.co";
 const SUPABASE_KEY = "sb_publishable_MN6PE8eCKzoJCUZJ6sC1CQ_JyGR95PZ";
